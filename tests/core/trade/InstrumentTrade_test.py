e38af5a59f7cfa685f5593f10e04cc874f81edcd
Eugene The Red <me@eugenethered.com>
2022-04-13 12:16:16 +0100

    fixing typo

--- tests/core/trade/InstrumentTrade_test.py
+++ tests/core/trade/InstrumentTrade_test.py
@@ -11,7 +11,7 @@ class InstrumentTradeTestCase(unittest.TestCase):
         self.assertEqual('BTC', trade.instrument_to)
         self.assertEqual(100, trade.quantity)
 
-    def test_should_have_instruments_to_trade_with_fractional_quanities(self):
+    def test_should_have_instruments_to_trade_with_fractional_quantity(self):
         trade = InstrumentTrade('BTC', 'USDT', 0.0025)
         self.assertEqual('BTC', trade.instrument_from)
         self.assertEqual('USDT', trade.instrument_to)
