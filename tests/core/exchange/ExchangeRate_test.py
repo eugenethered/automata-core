cb89b640b71872770372c73f77ad60413e86a716
Eugene the Red <me@eugenethered.com>
2022-07-30 14:58:04 +0100

    exchange rate baked directly

--- tests/core/exchange/ExchangeRate_test.py
+++ tests/core/exchange/ExchangeRate_test.py
@@ -18,6 +18,18 @@ class ExchangeRateTestCase(unittest.TestCase):
         self.assertEqual('USDT', to_instrument)
         self.assertEqual(BigFloat('38882.51'), rate)
 
+    def test_exchange_rate_inverse_rate(self):
+        exchange_rate = ExchangeRate(instrument='BTC', to_instrument='USDT', rate=BigFloat('38882.51'))
+        inverse_rate = exchange_rate.inverse()
+        self.assertEqual(BigFloat('38882.51'), exchange_rate.rate)
+        self.assertEqual(BigFloat('0.000025718504283802'), inverse_rate)
+
+    def test_exchange_rate_inverse_rate_from_smaller(self):
+        exchange_rate = ExchangeRate(instrument='USDT', to_instrument='BTC', rate=BigFloat('0.000025718504283802'))
+        inverse_rate = exchange_rate.inverse()
+        self.assertEqual(BigFloat('0.000025718504283802'), exchange_rate.rate)
+        self.assertEqual(BigFloat('38882.510000001006939583'), inverse_rate)
+
 
 if __name__ == '__main__':
     unittest.main()
