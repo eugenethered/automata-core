86c1247640fb117bfc3f3505bd16b362918b9a86
Eugene The Red <me@eugenethered.com>
2022-09-10 16:45:29 +0100

    refacting big float

--- core/number/BigFloat.py
+++ core/number/BigFloat.py
@@ -12,23 +12,31 @@ class BigFloat:
 
     def parse_args(self, args):
         if len(args) > 1:
-            self.number = int(args[0])
-            self.decimals = int(args[1])
+            self.__parse_number_decimal(args)
         else:
             value_stringed = str(args[0])
-            scientific_notation_pos = value_stringed.find('e-')
-            if scientific_notation_pos > -1:
-                (number_value, number_to_shift) = value_stringed.split('e-')
-                add_point = 1 if number_value.find('.') > 0 else 0
-                self.number = int(number_value.replace('.', ''))
-                self.decimals = int(number_to_shift) + add_point
+            if value_stringed.find('e-') > -1:
+                self.__parse_scientific_notion(value_stringed)
             else:
-                decimal_marker_pos = value_stringed.find('.')
-                if decimal_marker_pos > -1:
-                    self.number = int(value_stringed.replace('.', ''))
-                    self.decimals = len(value_stringed) - decimal_marker_pos - 1
-                else:
-                    self.number = int(value_stringed)
+                self.__parse_decimal_string(value_stringed)
+
+    def __parse_number_decimal(self, args):
+        self.number = int(args[0])
+        self.decimals = int(args[1])
+
+    def __parse_scientific_notion(self, value_stringed):
+        (number_value, number_to_shift) = value_stringed.split('e-')
+        add_point = 1 if number_value.find('.') > 0 else 0
+        self.number = int(number_value.replace('.', ''))
+        self.decimals = int(number_to_shift) + add_point
+
+    def __parse_decimal_string(self, value_stringed):
+        decimal_marker_pos = value_stringed.find('.')
+        if decimal_marker_pos > -1:
+            self.number = int(value_stringed.replace('.', ''))
+            self.decimals = len(value_stringed) - decimal_marker_pos - 1
+        else:
+            self.number = int(value_stringed)
 
     def add(self, other):
         if self.decimals == other.decimals:
